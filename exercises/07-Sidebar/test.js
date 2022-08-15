33359be7f310a3709064582b5455772f7a63063c
ErnestoXG <ernestox.061@gmail.com>
2022-08-15 20:45:01 +0000

    added test folders

---
+++ exercises/07-Sidebar/test.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+document.documentElement.innerHTML = html.toString();
+
+jest.dontMock('fs');
