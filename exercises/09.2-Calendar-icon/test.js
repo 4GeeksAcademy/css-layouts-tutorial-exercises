98cd5d87e4b670c5ec68f46b1acb5f6af9e42f39
ErnestoXG <ernestox.061@gmail.com>
2022-09-01 19:46:25 +0000

    unfinished test

--- exercises/09.2-Calendar-icon/test.js
+++ exercises/09.2-Calendar-icon/test.js
@@ -1,6 +1,53 @@
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+const css = fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
 document.documentElement.innerHTML = html.toString();
 
 jest.dontMock('fs');
+
+const link = document.querySelector("link");
+const title = document.querySelector('title')
+let p = document.querySelector("p")
+
+
+test("You should not change the existing head tag elements", () => {
+    let head = document.querySelector('head')
+    expect(head).toBeTruthy()
+
+    let meta = head.querySelector("meta")
+    expect(meta).toBeTruthy()
+
+    const href = link.getAttribute("href")
+    expect(href).toBe('styles.css')
+
+    expect(title).toBeTruthy()
+})
+
+test("p tag with class calendar should exist", () => {
+    expect(p.classList.contains("calendar")).toBeTruthy()
+})
+
+test("There should be an em tag inside the p tag", () => {
+    let em = p.querySelector("em")
+    expect(em).toBeTruthy()
+})
+
+test("calendar:before and calendar:after should have a position of absolute", () => {
+    document.querySelector(
+        "head"
+    ).innerHTML = `<style>${css.toString()}</style>`;
+
+    let cssArray = document.styleSheets[0].cssRules;
+
+    let position = ""
+
+    for (let i = 0; i < cssArray.length; i++) {
+        if (cssArray[i].selectorText === ".calendar::after" || cssArray[i].selectorText === ".calendar::before") {
+            position = cssArray[i].style[position];
+        }
+    }
+
+
+    expect(position).toBe("absolute")
+})
