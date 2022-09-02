17b4c4e6e17a2e451689b7d601260f19bb7b0b8c
ErnestoXG <ernestox.061@gmail.com>
2022-09-02 00:41:41 +0000

    Removed test.js, not convinient for this exercise

--- exercises/10-static-layout/test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
-document.documentElement.innerHTML = html.toString();
-
-jest.dontMock('fs');
